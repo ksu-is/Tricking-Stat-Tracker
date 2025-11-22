5b5c0b10c7be63473efa7eb831793ea6162cf922
Stray3rr <tstamps2@students.kennesaw.edu>
2025-11-21 19:42:45 -0500

    Display python
    Started the python coding for the displays. Will add the html goodies later

---
+++ display.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('store-achievement')
+def store_achievement():
+    return "Store Achievement"
+@app.route('current-achievements')
+def current_achievements():
+    return "Current Achievements"
+@app.route('set-goal')
+def set_goal():
+    return "Set Goal"
+
+if __name__=="__main__":
+    app.run(debug=True)
