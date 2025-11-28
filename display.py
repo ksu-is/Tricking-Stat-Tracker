cf23a06979ba257ef8e993843a001a1ce6526cc7
Stray3rr <tstamps2@students.kennesaw.edu>
2025-11-28 18:59:07 -0500

    Display fix
    Fixed display not working

--- display.py
+++ display.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('home.html')
-@app.route('store-achievement')
+@app.route("/store-achievement")
 def store_achievement():
     return "Store Achievement"
-@app.route('current-achievements')
+@app.route("/current-achievements")
 def current_achievements():
     return "Current Achievements"
-@app.route('set-goal')
+@app.route("/set-goal")
 def set_goal():
     return "Set Goal"
 
